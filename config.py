39a7c89fc8cb3cb63489ce6dc4823ffaeeb89bbd
Theodor Gheorghe <85551481+Theodor-Gheorghe@users.noreply.github.com>
2025-08-11 13:53:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-# Calea absolută către fișierul SQLite (salvat în folderul 'database')
+# The absolute path to the SQLite file (stored in the 'database' folder)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'database', 'requests.db')
